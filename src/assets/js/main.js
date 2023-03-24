2417ef48acd946e5f17271a5f407317e3512bb1f
tmoris <57036823+tmoris@users.noreply.github.com>
2023-03-24 14:45:50 +0400

    Changed form values const in main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -204,11 +204,13 @@ const constraints = {
 
 const form = document.getElementById('contact-form');
 form.addEventListener('submit', function (event) {
-  const formValues = {
-    name: form.elements.name.value,
-    email: form.elements.email.value,
-    message: form.elements.message.value
-  };
+ const formValues = {
+   name: form.elements.name.value,
+   email: form.elements.email.value,
+   subject: form.elements.subject.value,
+   message: form.elements.message.value,
+ };
+
 
   const errors = validate(formValues, constraints);
 
