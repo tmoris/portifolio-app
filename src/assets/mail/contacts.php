0c8c5ccf008383fd0d59846e396973682fa6cf70
tmoris <57036823+tmoris@users.noreply.github.com>
2023-03-24 14:44:44 +0400

    Removed email.js assets

---
+++ src/assets/mail/contacts.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "your_email@example.com";
+    $subject = "New message from your portfolio page";
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Your message was sent successfully!";
+    } else {
+        echo "There was a problem sending your message. Please try again.";
+    }
+}
+?>
