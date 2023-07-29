2692a710ac30f680f1cbbe984e506b9ffb586e9f
Tibenkana Moris <moristotau@gmail.com>
2023-07-29 16:57:42 +0400

    Created emil.js

--- src/assets/mail/email.js
+++ src/assets/mail/email.js
@@ -1,19 +1,36 @@
-import emailjs from 'emailjs-com';
-
-emailjs.init('U9h2cQeseMkxog38Q');
-
+console.log("Script is running");
 window.onload = function () {
+	emailjs.init("U9h2cQeseMkxog38Q");
+
 	document.getElementById('contact-form').addEventListener('submit', function (event) {
 		event.preventDefault();
 
-		const serviceId = 'service_r8k6kmz';
-		const templateId = 'template_qzujp2i';
+		let form = this;
+		let params = {
+			name: form.elements.name.value,
+			email: form.elements.email.value,
+			subject: form.elements.subject.value,
+			message: form.elements.message.value
+		};
 
-		emailjs.sendForm(serviceId, templateId, this)
-			.then(function () {
-				console.log('SUCCESS!');
+		let loadingDiv = document.querySelector('.loading');
+		let errorDiv = document.querySelector('.error-message');
+		let successDiv = document.querySelector('.success-message ');
+
+		loadingDiv.style.display = 'block';
+
+		emailjs.send('service_r8k6kmz', 'template_qzujp2i', params, "U9h2cQeseMkxog38Q")
+			.then(function (response) {
+				loadingDiv.style.display = 'none';
+				successDiv.style.display = 'block';
+				console.log('SUCCESS!', response);
+				// Clear the form.
+				form.reset();
 			}, function (error) {
+				loadingDiv.style.display = 'none';
+				errorDiv.innerHTML = 'Error: ' + error;
+				errorDiv.style.display = 'block';
 				console.log('FAILED...', error);
 			});
 	});
-}
+};
