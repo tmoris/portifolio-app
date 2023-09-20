ec432c23acbbfeebb62b1d1af6af2e18b465aae8
Tibenkana Moris <moristotau@gmail.com>
2023-09-20 16:31:54 +0400

    Removed  starting a development server, and live-reloading -gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,11 @@
-const { stream } = require('browser-sync');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const browserSync = require('browser-sync').create();
-
-// compile scss into css 
 
 function style() {
-  //   where is scss file
-  return gulp.src('.sass/**/*.scss')
-// path that file through sass compiler 
-  .pipe(sass().on('error', sass.logError))
-  // where do i save the compiled css 
-  .pipe(gulp.dest('./css'))
-
-  // stream changes to all browsers 
-.pipe(browserSync.stream());
+  return gulp
+    .src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
 }
 
-function watch() {
-  browserSync.init( {
-    server: {
-      baseDir: './'
-    }
-  });
-  gulp.watch('.sass/**/*.scss', style);
-  gulp.watch('./**/*.html').on('change', browserSync.reload);
-  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
-
-}
 exports.style = style;
-exports.watch = watch;
